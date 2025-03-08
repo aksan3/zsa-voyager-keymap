abc6bce9f1e43818149dfde62757278babedfca4
John Lee <shkblue@hotmail.com>
2025-03-07 23:16:32 -0800

    Add custom configurations from Q10.

--- e5rwR/config.h
+++ e5rwR/config.h
@@ -14,3 +14,17 @@
 #define TAPPING_TERM_PER_KEY
 #define RGB_MATRIX_STARTUP_SPD 60
 
+
+
+/*
+ * Begin custom configuration
+ */
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+// Timeout for caps word to turn off when no keys are pressed
+#define CAPS_WORD_IDLE_TIMEOUT 3000 // in ms
+// Temporarily un-shift characters in the middle of caps word
+#define CAPS_WORD_INVERT_ON_SHIFT
+// Allow shifted state to repeat by holding key beyond AUTO_SHIFT_TIMEOUT
+#define AUTO_SHIFT_REPEAT
